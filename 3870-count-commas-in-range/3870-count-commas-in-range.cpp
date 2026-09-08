6272c8a88cb73b77aa53bd7d84d4a5909899d1c7
Dheeraj Singh <145746734+Dheeraj261708@users.noreply.github.com>
2026-09-08 23:54:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int countCommas(int n) {
-        return max(0, n - 999);
+        // return max(0, n - 999);
+        int commas = 0;
+        long long threshold = 1000;
+        
+        while (n >= threshold) {
+            commas += (n - threshold + 1);
+            threshold *= 1000;
+        }
+        
+        return commas;
     }
 };
